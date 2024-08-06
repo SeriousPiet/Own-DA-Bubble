c0c0000e95a2e9145c3db5241ec14e16a16b1bd5
Anthony Hamon <anthony.hamon91@gmail.com>
2024-08-06 19:40:51 +0200

    feat: #5 added message textarea componnent

--- src/app/chatcontent/chatview/chatview.component.ts
+++ src/app/chatcontent/chatview/chatview.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MessageComponent } from './message/message.component';
 import { MessageDateComponent } from './message-date/message-date.component';
+import { MessageTextareaComponent } from '../message-textarea/message-textarea.component';
 
 @Component({
   selector: 'app-chatview',
   standalone: true,
-  imports: [MessageDateComponent, MessageComponent],
+  imports: [MessageDateComponent, MessageComponent, MessageTextareaComponent],
   templateUrl: './chatview.component.html',
   styleUrl: './chatview.component.scss'
 })
