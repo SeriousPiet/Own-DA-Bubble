a28d55d25ab470e1edee4518790295ee3958690d
Anthony Hamon <anthony.hamon91@gmail.com>
2024-08-08 16:26:47 +0200

    style: #5 finished styling icons in message-textareas component with hover effect etc

--- src/app/chatcontent/message-textarea/message-textarea.component.ts
+++ src/app/chatcontent/message-textarea/message-textarea.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message-textarea',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './message-textarea.component.html',
   styleUrl: './message-textarea.component.scss'
 })
 export class MessageTextareaComponent {
 
+  isHovered = false;
+  isActive = false;
+
+  
 }
