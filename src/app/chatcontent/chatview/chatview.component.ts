ff20983644573d4308d0d35f6af1265022e59771
Anthony Hamon <anthony.hamon91@gmail.com>
2024-08-12 18:56:55 +0200

    feature: #20 added popups components in chatview

--- src/app/chatcontent/chatview/chatview.component.ts
+++ src/app/chatcontent/chatview/chatview.component.ts
@@ -3,11 +3,13 @@ import { MessageDateComponent } from './message-date/message-date.component';
 import { MessageTextareaComponent } from '../message-textarea/message-textarea.component';
 import { CommonModule } from '@angular/common';
 import { MessageComponent } from './message/message.component';
+import { PopoverChannelEditorComponent } from './popover-chatview/popover-channel-editor/popover-channel-editor.component';
+import { PopoverChannelMemberOverviewComponent } from './popover-chatview/popover-channel-member-overview/popover-channel-member-overview.component';
 
 @Component({
   selector: 'app-chatview',
   standalone: true,
-  imports: [MessageDateComponent, MessageComponent, MessageTextareaComponent, CommonModule],
+  imports: [CommonModule, MessageDateComponent, MessageComponent, MessageTextareaComponent, PopoverChannelEditorComponent, PopoverChannelMemberOverviewComponent  ],
   templateUrl: './chatview.component.html',
   styleUrl: './chatview.component.scss'
 })
