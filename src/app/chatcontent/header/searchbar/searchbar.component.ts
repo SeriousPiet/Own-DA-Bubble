4fa69f65bc18b0f79e6a666c7b073d3efbc64101
CodeRacon <michael.buschmann.86@gmail.com>
2024-08-19 00:42:05 +0200

    feat/style: #24 implemented logic to provide option to seach within a given context, chosen by the user, otherwise context is global; added Lifecycle Hooks, Event Handlers, UI Interaction Methods, Recent Searches Management, Utility Methods

--- src/app/chatcontent/header/searchbar/searchbar.component.ts
+++ src/app/chatcontent/header/searchbar/searchbar.component.ts
@@ -1,12 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SearchService } from '../../../utils/services/search.service';
+import { NavigationService } from '../../../utils/services/navigation.service';
+import { Channel } from '../../../shared/models/channel.class';
+import { Chat } from '../../../shared/models/chat.class';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './searchbar.component.html',
-  styleUrl: './searchbar.component.scss'
+  styleUrls: ['./searchbar.component.scss'],
 })
-export class SearchbarComponent {
+export class SearchbarComponent implements OnInit {
+  searchState$!: Observable<{
+    query: string;
+    context: string | null;
+    keywords: string[];
+  }>;
+  searchQuery: string = '';
+  suggestions$!: Observable<string[]>;
+  isDropdownVisible = false;
+  recentSearches: string[] = [];
+  currentContext: string = '';
 
+  // Lifecycle Hooks
+  constructor(
+    private searchService: SearchService,
+    public navigationService: NavigationService
+  ) {}
+
+  ngOnInit() {
+    this.suggestions$ = this.searchService.getSearchSuggestions();
+    this.currentContext = this.searchService.getCurrentContext();
+  }
+
+  // Event Handlers
+  onSearchInput() {
+    this.searchService.updateSearchQuery(this.searchQuery);
+    this.suggestions$ = this.searchService.getSearchSuggestions();
+    console.log('Searching for:', this.searchQuery);
+  }
+
+  onFocus() {
+    this.currentContext = this.searchService.getCurrentContext();
+    this.isDropdownVisible = true;
+
+    const chatViewObject = this.navigationService.chatViewObject;
+    if (chatViewObject instanceof Channel) {
+      console.log('Current chatViewObject name:', chatViewObject.name);
+    } else if (chatViewObject instanceof Chat) {
+      console.log('Current chatViewObject is a Chat');
+    } else {
+      console.log('Current chatViewObject is undefined or unknown type');
+    }
+    console.log('Current context:', this.getCurrentContext());
+  }
+
+  onBlur() {
+    setTimeout(() => {
+      this.isDropdownVisible = false;
+    }, 200);
+  }
+
+  // UI Interaction Methods
+  selectSuggestion(suggestion: string) {
+    this.searchQuery = suggestion;
+    this.searchService.updateSearchQuery(suggestion);
+    this.isDropdownVisible = false;
+  }
+
+  addContextToSearch() {
+    const context = this.searchService.getCurrentContext();
+    if (context) {
+      this.searchService.addContextRestriction(context);
+    }
+  }
+
+  removeContextFromSearch() {
+    this.searchService.removeContextRestriction();
+  }
+
+  addKeyword(keyword: string) {
+    this.searchService.addKeywordRestriction(keyword);
+  }
+
+  removeKeyword(keyword: string) {
+    this.searchService.removeKeywordRestriction(keyword);
+  }
+
+  // Recent Searches Management
+  addSearchTerm(term: string) {
+    this.searchService.addRecentSearch(term);
+    this.recentSearches = this.searchService.getRecentSearches();
+  }
+
+  removeSearchTerm(term: string) {
+    this.searchService.removeRecentSearch(term);
+    this.recentSearches = this.searchService.getRecentSearches();
+  }
+  // Utility Methods
+  getActiveRestrictions() {
+    return this.searchService.getSearchRestrictions();
+  }
+
+  getCurrentContext(): string {
+    return this.searchService.getCurrentContext();
+  }
 }
