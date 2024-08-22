fbe55f026c9cdda1dd23e2f5b90ec0878d461650
CodeRacon <michael.buschmann.86@gmail.com>
2024-08-22 20:52:48 +0200

    refactor/doc: #24 revised functionality, size and usage of methods; refactored a vast amoount; deleted redundant methods; added jsdoc to all searchbar-refered files

--- src/app/utils/services/search.service.ts
+++ src/app/utils/services/search.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import {
   map,
-  tap,
   switchMap,
   debounceTime,
   distinctUntilChanged,
@@ -10,6 +9,7 @@ import {
 
 import { NavigationService } from './navigation.service';
 import { UsersService } from './user.service';
+import { ChannelService } from './channel.service';
 
 import {
   Firestore,
@@ -25,14 +25,19 @@ import { collectionData } from 'rxfire/firestore';
 })
 export class SearchService {
   private recentSearches: string[] = [];
+  private readonly RECENT_SEARCHES_KEY = 'recentSearches';
+  private _isContextSearchEnabled: boolean = false;
+
+  get isContextSearchEnabled(): boolean {
+    return this._isContextSearchEnabled;
+  }
+
   private searchStateSubject = new BehaviorSubject<{
     query: string;
     context: string | null;
-    keywords: string[];
   }>({
     query: '',
     context: null,
-    keywords: [],
   });
 
   searchState$ = this.searchStateSubject.asObservable();
@@ -40,10 +45,26 @@ export class SearchService {
   constructor(
     private navigationService: NavigationService,
     private firestore: Firestore,
-    private usersService: UsersService
-  ) {}
+    private usersService: UsersService,
+    private channelService: ChannelService
+  ) {
+    console.log(
+      'Verfügbare Channels im SearchService:',
+      this.channelService.channels
+    );
+  }
 
+  // ############################################################################################################
   // State Management Methods
+  // ############################################################################################################
+
+  /**
+   * Updates the search query in the current search state.
+   *
+   * This method updates the search state by setting the query to the provided value.
+   *
+   * @param query - The new search query to set in the search state.
+   */
   updateSearchQuery(query: string) {
     this.searchStateSubject.next({
       ...this.searchStateSubject.value,
@@ -51,6 +72,14 @@ export class SearchService {
     });
   }
 
+  /**
+   * Adds a context restriction to the current search state.
+   *
+   * This method updates the search state by setting the context to the provided value,
+   * effectively adding a context restriction to the search.
+   *
+   * @param context - The context to add to the search state.
+   */
   addContextRestriction(context: string) {
     this.searchStateSubject.next({
       ...this.searchStateSubject.value,
@@ -58,6 +87,12 @@ export class SearchService {
     });
   }
 
+  /**
+   * Removes the current context restriction from the search state.
+   *
+   * This method updates the search state by setting the context to `null`,
+   * effectively removing any previous context restriction.
+   */
   removeContextRestriction() {
     this.searchStateSubject.next({
       ...this.searchStateSubject.value,
@@ -65,122 +100,238 @@ export class SearchService {
     });
   }
 
-  addKeywordRestriction(keyword: string) {
-    const currentKeywords = this.searchStateSubject.value.keywords;
-    if (!currentKeywords.includes(keyword)) {
-      this.searchStateSubject.next({
-        ...this.searchStateSubject.value,
-        keywords: [...currentKeywords, keyword],
-      });
-    }
-  }
-
-  removeKeywordRestriction(keyword: string) {
-    const currentKeywords = this.searchStateSubject.value.keywords;
-    this.searchStateSubject.next({
-      ...this.searchStateSubject.value,
-      keywords: currentKeywords.filter((k) => k !== keyword),
-    });
-  }
-
+  // ############################################################################################################
   // Search Methods
+  // ############################################################################################################
+
+  /**
+   * Adds the provided search term to the list of recent searches.
+   *
+   * This method retrieves the current list of recent searches from local storage,
+   * adds the new search term to the beginning of the list, and then trims the list
+   * to a maximum of 5 entries before storing it back in local storage.
+   *
+   * @param term - The search term to add to the list of recent searches.
+   */
   addRecentSearch(term: string) {
-    if (!this.recentSearches.includes(term)) {
-      this.recentSearches.unshift(term);
-      if (this.recentSearches.length > 5) {
-        this.recentSearches.pop();
-      }
-    }
+    if (term.trim() === '') return; // Ignoriere leere Suchanfragen
+    let searches = this.getRecentSearches();
+    searches = [term, ...searches.filter((s) => s !== term)].slice(0, 5);
+    localStorage.setItem(this.RECENT_SEARCHES_KEY, JSON.stringify(searches));
   }
 
+  /**
+   * Retrieves the list of recent searches from local storage.
+   *
+   * This method reads the recent searches from the local storage and returns them as an array of strings.
+   * If no recent searches are found in local storage, an empty array is returned.
+   *
+   * @returns An array of recent search terms.
+   */
   getRecentSearches(): string[] {
-    return this.recentSearches;
+    const searches = localStorage.getItem(this.RECENT_SEARCHES_KEY);
+    return searches ? JSON.parse(searches) : [];
   }
 
+  /**
+   * Removes the specified search term from the list of recent searches.
+   *
+   * This method retrieves the current list of recent searches from local storage,
+   * filters out the provided search term, and then stores the updated list back
+   * in local storage.
+   *
+   * @param term - The search term to remove from the list of recent searches.
+   */
   removeRecentSearch(term: string) {
     this.recentSearches = this.recentSearches.filter((t) => t !== term);
   }
 
-  getSearchSuggestions(): Observable<string[]> {
+  getSearchSuggestions(): Observable<
+    { text: string; type: string; hasChat: boolean }[]
+  > {
     return this.searchState$.pipe(
       debounceTime(300),
       distinctUntilChanged(),
       switchMap((state) => {
-        console.log('Current search state:', state);
-
-        if (state.context && state.context.startsWith('in:#')) {
-          console.log('Searching in channel');
-          return this.searchInChannel(state.query, state.context);
-        } else if (state.context && state.context.startsWith('in:@')) {
-          console.log('Searching in DM');
-          return this.searchInDM(state.query, state.context);
+        if (!state.query) {
+          return of([]);
+        }
+        if (this.isContextSearchEnabled && state.context) {
+          return this.searchWithContext(state.query, state.context);
         } else {
-          console.log('Performing global search');
           return this.searchGlobal(state.query);
         }
+      })
+    );
+  }
+
+  /**
+   * Searches for global search suggestions based on the provided query.
+   *
+   * @param query - The search query to match against user names and channel names.
+   * @returns An observable that emits an array of search suggestions, where each suggestion has a `text`, `type`, and `hasChat` property.
+   * The `text` property contains the user name or channel name, the `type` property indicates whether the suggestion is for a user or a channel,
+   * and the `hasChat` property indicates whether the suggestion has an associated chat.
+   */
+  private searchGlobal(
+    query: string
+  ): Observable<{ text: string; type: string; hasChat: boolean }[]> {
+    return of(this.channelService.channels).pipe(
+      map((channels) => {
+        const users = this.usersService.getAllUserIDs().map((userId) => {
+          const user = this.usersService.getUserByID(userId);
+          return {
+            text: `@${user?.name}`,
+            type: 'user',
+            hasChat:
+              this.usersService.getChatWithUserByID(userId, false) !==
+              undefined,
+          };
+        });
+
+        const channelResults = channels.map((channel) => ({
+          text: `#${channel.name}`,
+          type: 'channel',
+          hasChat: true,
+        }));
+
+        return [...users, ...channelResults];
       }),
-      tap((results) => console.log('Search results:', results))
+      map((results) =>
+        results.filter((item) =>
+          item.text.toLowerCase().includes(query.toLowerCase())
+        )
+      )
     );
   }
 
-  private searchInChannel(
+  /**
+   * Searches for search suggestions based on the provided query and the current search context.
+   *
+   * @param query - The search query to match against user names and channel names.
+   * @param context - The current search context, which can be a channel or a user.
+   * @returns An observable that emits an array of search suggestions, where each suggestion has a `text`, `type`, and `hasChat` property.
+   * The `text` property contains the user name or channel name, the `type` property indicates whether the suggestion is for a user or a channel,
+   * and the `hasChat` property indicates whether the suggestion has an associated chat.
+   */
+  private searchWithContext(
     query: string,
-    channelContext: string
-  ): Observable<string[]> {
-    const channelId = channelContext.replace('in:#', '');
-    const messagesRef = collection(
-      this.firestore,
-      `channels/${channelId}/messages`
-    );
-    const q = firestoreQuery(
-      messagesRef,
-      where('content', '>=', query),
-      where('content', '<=', query + '\uf8ff'),
-      limit(5)
-    );
-
-    return collectionData(q).pipe(
-      map((messages) => messages.map((msg) => msg['content']))
+    context: string
+  ): Observable<{ text: string; type: string; hasChat: boolean }[]> {
+    return of(this.channelService.channels).pipe(
+      map((channels) => {
+        if (context.startsWith('in:#')) {
+          const channelName = context.slice(4);
+          const channel = channels.find((c) => c.name === channelName);
+          if (channel) {
+            return channel.members.map((userId) => {
+              const user = this.usersService.getUserByID(userId);
+              return {
+                text: `@${user?.name}`,
+                type: 'user',
+                hasChat:
+                  this.usersService.getChatWithUserByID(userId, false) !==
+                  undefined,
+              };
+            });
+          }
+        } else if (context.startsWith('in:@')) {
+          const userName = context.slice(4);
+          const user = this.usersService
+            .getAllUserIDs()
+            .find((id) => this.usersService.getUserByID(id)?.name === userName);
+          if (user) {
+            return [
+              {
+                text: `@${this.usersService.getUserByID(user)?.name}`,
+                type: 'user',
+                hasChat: true,
+              },
+            ];
+          }
+        }
+        return [];
+      }),
+      map((results) =>
+        results.filter((item) =>
+          item.text.toLowerCase().includes(query.toLowerCase())
+        )
+      )
     );
   }
 
-  private searchInDM(query: string, dmContext: string): Observable<string[]> {
-    const dmId = dmContext.replace('in:@', '');
-    const messagesRef = collection(this.firestore, `dms/${dmId}/messages`);
-    const q = firestoreQuery(
-      messagesRef,
-      where('content', '>=', query),
-      where('content', '<=', query + '\uf8ff'),
-      limit(5)
-    );
-
-    return collectionData(q).pipe(
-      map((messages) => messages.map((msg) => msg['content']))
-    );
-  }
-
-  private searchGlobal(query: string): Observable<string[]> {
-    const registeredUsers = this.getRegisteredUsers();
-    console.log('Registered Users:', registeredUsers);
-
-    return of([
-      `Globale Suche für: ${query}`,
-      ...registeredUsers.filter((user) =>
-        user.toLowerCase().includes(query.toLowerCase())
-      ),
-    ]);
-  }
-
+  // ############################################################################################################
   // Utility Methods
+  // ############################################################################################################
+
+  setContextSearchEnabled(enabled: boolean) {
+    this._isContextSearchEnabled = enabled;
+  }
+
+  /**
+   * Gets the current search context.
+   *
+   * @returns The current search context.
+   */
   getCurrentContext(): string {
     return this.navigationService.getSearchContext();
   }
 
+  /**
+   * Gets the current search restrictions.
+   *
+   * @returns The current search restrictions.
+   */
   getSearchRestrictions() {
     return this.searchStateSubject.value;
   }
 
+  /**
+   * Gets a list of all registered user names.
+   *
+   * @returns An array of strings representing the names of all registered users. If a user's name is not available, the string 'Unbekannter Benutzer'
+   * (German for 'Unknown User') is used instead.
+   */
   getRegisteredUsers(): string[] {
-    return this.usersService.getAllUserIDs();
+    return this.usersService.getAllUserIDs().map((id) => {
+      const user = this.usersService.getUserByID(id);
+      return user ? user.name : 'Unbekannter Benutzer';
+    });
+  }
+
+  /**
+   * Gets the members of the current search context, if the context is a channel.
+   *
+   * @returns An array of strings representing the member names of the current channel, or an empty array if the current context is not a channel.
+   */
+  getContextMembers(): string[] {
+    const context = this.getCurrentContext();
+    console.log('Aktueller Kontext:', context);
+    if (context.startsWith('in:#')) {
+      const channelName = context.slice(4);
+      console.log('Suche nach Channel:', channelName);
+      console.log('Verfügbare Channels:', this.channelService.channels);
+      const channel = this.channelService.channels.find(
+        (c) => c.name === channelName
+      );
+      console.log('Gefundener Channel:', channel);
+      return channel ? channel.members : [];
+    }
+    return [];
+  }
+
+  /**
+   * Gets a list of user names for the provided user IDs.
+   *
+   * @param userIDs - An array of user IDs.
+   * @returns An array of user names. If a user's name is not available, an empty string is returned for that user.
+   */
+  getUserNames(userIDs: string[]): string[] {
+    return userIDs
+      .map((id) => {
+        const user = this.usersService.getUserByID(id);
+        return user ? user.name : '';
+      })
+      .filter((name) => name !== '');
   }
 }
