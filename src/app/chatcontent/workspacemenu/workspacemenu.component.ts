96b6452eb9db1f0b46ff2b8918ff9fa2f1f438cf
CodeRacon <michael.buschmann.86@gmail.com>
2024-08-24 14:59:50 +0200

    test/feat #25 #6 implemented AvatarDirective for testing purpose

--- src/app/chatcontent/workspacemenu/workspacemenu.component.ts
+++ src/app/chatcontent/workspacemenu/workspacemenu.component.ts
@@ -7,11 +7,18 @@ import { User } from '../../shared/models/user.class';
 import { ChannelService } from '../../utils/services/channel.service';
 import { Channel } from '../../shared/models/channel.class';
 import { NavigationService } from '../../utils/services/navigation.service';
+import { AvatarDirective } from '../../utils/directives/avatar.directive';
 
 @Component({
   selector: 'app-workspacemenu',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AvatarDirective,
+  ],
   templateUrl: './workspacemenu.component.html',
   styleUrl: './workspacemenu.component.scss',
 })
