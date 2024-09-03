44c619a7dcdacae0183ad03c574f596c211d4d8c
Peter <Peter.Kormann1@googlemail.com>
2024-09-03 17:52:01 +0200

    refactor: merged add-channel-searchbar.component to addChannel.component, added Button functions to disable and activate it, deleted add-channel-searchbar.component #27  #15

--- src/app/chatcontent/workspacemenu/workspacemenu.component.ts
+++ src/app/chatcontent/workspacemenu/workspacemenu.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { UsersService } from '../../utils/services/user.service';
@@ -25,6 +25,7 @@ import { AddchannelComponent } from '../../chatcontent/workspacemenu/addchannel/
   styleUrl: './workspacemenu.component.scss',
 })
 export class WorkspacemenuComponent implements OnInit {
+  addChannelId: HTMLElement | null = null;
   public userservice = inject(UsersService);
   public channelservice = inject(ChannelService);
   public users: User[] = [];
@@ -32,10 +33,21 @@ export class WorkspacemenuComponent implements OnInit {
   public onlineStatus: string = 'offline';
   public onlineColor: string = '#92c83e';
   public offlineColor: string = '#686868';
-
   private navigationService = inject(NavigationService);
 
+  @ViewChild(AddchannelComponent) addChannelComponent!: AddchannelComponent;
+
   ngOnInit(): void {}
+
+  toggleAddChannelPopover() {
+    this.addChannelId = document.getElementById('addChannelId');
+    this.addChannelId?.showPopover();
+    this.addChannelComponent.resetAddChannel();
+    if (!this.addChannelComponent.toggleAddChannelPopover) {
+      this.addChannelComponent.toggleAddChannelPopover =
+        !this.addChannelComponent.toggleAddChannelPopover;
+    }
+  }
 
   setCurrentChannel(newChannel: Channel) {
     this.navigationService.setChatViewObject(newChannel);
