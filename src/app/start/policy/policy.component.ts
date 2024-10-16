066f6b57f8242843574d734d5e2cf7f9f89d00f5
Anthony Hamon <anthony.hamon91@gmail.com>
2024-10-16 22:25:04 +0200

    fix: #28, links not routing to content but return notfound page 404

--- src/app/start/policy/policy.component.ts
+++ src/app/start/policy/policy.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Location } from '@angular/common';
 
 @Component({
@@ -9,10 +9,21 @@ import { Location } from '@angular/common';
   templateUrl: './policy.component.html',
   styleUrl: './policy.component.scss',
 })
-export class PolicyComponent {
-  constructor(private location: Location) {}
+export class PolicyComponent implements OnInit {
+  constructor(private location: Location, private route: ActivatedRoute) {}
 
   goBack() {
     this.location.back();
+  }
+
+  ngOnInit() {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
   }
 }
